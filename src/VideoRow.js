1a0fe6ccdfd4402ce55828b10fbaa96cbaed15a9
ABhati <abhaybhati121@gmail.com>
2022-10-22 20:34:41 +0530

    fix: remove alt

--- src/VideoRow.js
+++ src/VideoRow.js
@@ -1,25 +1,31 @@
-import './VideoRow.css';
-import Avatar from '@material-ui/core/Avatar';
+import "./VideoRow.css";
+import Avatar from "@material-ui/core/Avatar";
 
-import React from 'react'
+import React from "react";
 
-function VideoRow({image, title, views, timestamp, image2, channel, desc}) {
-    return (
-        <div className='videoRow'>
-            <div className = 'videoRow__thumbnail'>
-                <img src={image} alt='Thumbnail Image' />
-            </div>
-            <div className = 'videoRow__info'>
-                <h3>{title}</h3>
-                <p>{views} views . {timestamp}</p>
-                <div>
-                    <Avatar className = 'videoRow__info__avatar' src={image2} alt='Channel Image' />
-                    <p>{channel}</p>
-                </div>
-                <p>{desc}</p>
-            </div>
+function VideoRow({ image, title, views, timestamp, image2, channel, desc }) {
+  return (
+    <div className="videoRow">
+      <div className="videoRow__thumbnail">
+        <img src={image} />
+      </div>
+      <div className="videoRow__info">
+        <h3>{title}</h3>
+        <p>
+          {views} views . {timestamp}
+        </p>
+        <div>
+          <Avatar
+            className="videoRow__info__avatar"
+            src={image2}
+            alt="Channel Image"
+          />
+          <p>{channel}</p>
         </div>
-    )
+        <p>{desc}</p>
+      </div>
+    </div>
+  );
 }
 
-export default VideoRow
+export default VideoRow;
